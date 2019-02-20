96ab0b35ebac2b6d8c386394020e8e5ba9688172
Peter Domokos <peter.domokos@ni.com>
2019-02-20 14:52:44 +0100

    Refactoring - bidirectional relations

--- src/main/java/hu/atsoft/playground/springrecipe/domain/Ingredient.java
+++ src/main/java/hu/atsoft/playground/springrecipe/domain/Ingredient.java
@@ -22,12 +22,19 @@ public class Ingredient {
     public Ingredient() {
     }
 
-    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
         this.description = description;
         this.amount = amount;
         this.uom = uom;
-        this.recipe = recipe;
     }
+
+
+//    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
+//        this.description = description;
+//        this.amount = amount;
+//        this.uom = uom;
+//        this.recipe = recipe;
+//    }
 
     public Long getId() {
         return id;
