326c7fbbd12d758e4d2e747128c9b878fc975b77
dp <hudomokp@gmail.com>
2019-03-03 19:52:51 +0100

    Added test for RecipeServiceImpl

---
+++ src/test/java/hu/atsoft/playground/springrecipe/services/RecipeServiceImplTest.java
@@ -0,0 +1,42 @@
+package hu.atsoft.playground.springrecipe.services;
+
+import hu.atsoft.playground.springrecipe.domain.Recipe;
+import hu.atsoft.playground.springrecipe.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class RecipeServiceImplTest {
+
+    private RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() {
+        Recipe recipe = new Recipe();
+        Set<Recipe> recipesData = new HashSet<>();
+        recipesData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+        assertEquals(recipes.size(), 1);
+
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
